75910bd0094c82183b21a584bdc8cc07ce33261c
Pavlik1400 <pavlikhiley@gmail.com>
2021-04-30 21:27:26 +0300

    Fix bugs

---
+++ CREATE_TABLES.sql
@@ -0,0 +1,81 @@
+
+
+CREATE TABLE alcoholic (
+	id_alc SERIAL PRIMARY KEY,
+	fname varchar(30),
+	lname varchar(30)
+);
+
+CREATE TABLE inspector (
+	id_ins SERIAL PRIMARY KEY,
+	fname varchar(30),
+	lname varchar(30)
+);
+
+CREATE TABLE bed (
+	id_bed SERIAL PRIMARY KEY,
+	description varchar(200)
+);
+
+CREATE TABLE escape (
+	id_esc SERIAL PRIMARY KEY,
+	id_alc INTEGER NOT NULL,
+	escape_date TIMESTAMP NOT NULL,
+	bribe_id INTEGER
+);
+
+CREATE TABLE log (
+	id_log SERIAL PRIMARY KEY,
+	id_alc integer not null,
+	id_insp_out integer,
+	id_bed integer not null,
+	id_group_check_in integer not null, 
+	end_date timestamp not null
+);
+
+CREATE TABLE group_info (
+	id_group SERIAL PRIMARY KEY,
+	group_name varchar(100)
+);
+
+CREATE TABLE active_alcoholic (
+	id_act SERIAL PRIMARY KEY,
+  	id_alc integer not null,
+	id_bed integer not null,
+	id_group_check_in integer not null  	
+);
+
+CREATE TABLE bribe (
+	id_bribe SERIAL PRIMARY KEY,
+	id_ins integer not null,
+  price integer not null
+);
+
+CREATE TABLE group_check_in (
+	id_check_in SERIAL PRIMARY KEY,
+	id_group integer, 
+	time timestamp,
+	id_drink integer,
+	inspector_in integer not null
+);
+
+CREATE TABLE drink (
+	id_drink SERIAL PRIMARY KEY,
+	drink_name varchar(100) not null,
+	price integer
+);
+
+CREATE TABLE unconsciousness(
+	id_unc SERIAL not null,
+	id_alc integer not null,
+	begin_time timestamp,
+	end_time timestamp
+);
+
+CREATE TABLE job (
+	id_job serial not null,
+	id_alc integer not null,
+	job_name varchar(50),
+	begin_time timestamp not null,
+	end_time timestamp not null
+);
--- requests/6-12queries.sql
+++ requests/6-12queries.sql
@@ -1,40 +1,53 @@
-• знайти усiх алкоголiкiв, яких забирали у витверезник хоча б N разiв за вказаний перiод (з дати F по дату T);
+-- • знайти усiх алкоголiкiв, яких забирали у витверезник хоча б N разiв за вказаний перiод (з дати F по дату T);
+select * from group_check_in
+select * from log
 
-	SELECT id_alc FROM log
+	SELECT id_alc FROM 
+	log JOIN group_check_in 
+		ON group_check_in.id_check_in = log.id_group_check_in
+	WHERE time >= '2020-04-29 12:00:00' AND end_date <= '2021-05-10 22:30:00'
+	
+	UNION
+	
+	SELECT id_alc FROM 
+	active_alcoholic JOIN group_check_in 
+		ON group_check_in.id_check_in = active_alcoholic.id_group_check_in
+	WHERE time >= '2020-04-29 12:00:00'
+	
 	GROUP BY id_alc
-	HAVING beg_date >= F AND end_date <= T AND COUNT(id_alc) >= N
+	HAVING COUNT(id_alc) >= 1
 
-	SELECT id_alc FROM active_alcoholic
-	GROUP BY id_alc
-	HAVING beg_date >= F AND COUNT(id_alc) >= N
+-- • знайти усi спiльнi подiї для алкоголiка A та iнспектора I за вказаний перiод (з дати F по дату T);
 
-• знайти усi спiльнi подiї для алкоголiка A та iнспектора I за вказаний перiод (з дати F по дату T);
+-- 	// Спільні події - це який інспектор випускав/забирав якого алкоголіка?
+-- 	// Тоді тут забирання
+-- select * from group_check_in where inspector_in = 4
+-- select * from log where id_group_check_in = 4
 
-	// Спільні події - це який інспектор випускав/забирав якого алкоголіка?
-	// Тоді тут забирання
-	SELECT id_alc, id_insp_in FROM log
-	WHERE beg_date >= F AND end_date <= T AND id_insp_in == I
+	SELECT id_alc, inspector_in FROM 
+	log JOIN group_check_in 
+		ON group_check_in.id_check_in = log.id_group_check_in
+	WHERE time >= '2020-04-29 12:00:00' AND end_date <= '2021-05-10 22:30:00' 
+		  AND (inspector_in = 4 or id_insp_out = 4) AND id_alc = 7
+		 
+	UNION
+	
+	SELECT id_alc, inspector_in FROM 
+	active_alcoholic JOIN group_check_in 
+		ON group_check_in.id_check_in = active_alcoholic.id_group_check_in
+	WHERE time >= '2020-04-29 12:00:00' AND (inspector_in = 4) AND id_alc = 7
 
-	SELECT id_alc, inspector_in
-	FROM active_alcoholic
-	JOIN group
-	ON group.id_group = active_alcoholic.group_id
-	WHERE beg_date >= F AND inspector_in == I
 
+-- 	// Можна буде організувати табличку з історією алкоголіка і його перетин з інспекторами
 
-	// А тут випускання
-	SELECT id_alc, id_insp_out FROM log
-	WHERE beg_date >= F AND end_date <= T AND id_insp_out == I
-
-	// Можна буде організувати табличку з історією алкоголіка і його перетин з інспекторами
-
-• для алкоголiка A та кожного спиртого напою, що вiн вживав, знайти скiльки разiв за вказаний перiод (з дати F по дату T) вiн розпивав напiй у групi з щонайменше N алкоголiкiв; ??
+-- • для алкоголiка A та кожного спиртого напою, що вiн вживав, знайти скiльки разiв за вказаний 
+--   перiод (з дати F по дату T) вiн розпивав напiй у групi з щонайменше N алкоголiкiв; ??
 
 	
 	SELECT id_alc, id_drink, LENGTH (group_len)
 	FROM log
-	JOIN group
-	ON log.group_id = group.group_id
+	JOIN group_check_in
+	ON log.group_id = group_check_in.group_id
 	GROUP BY id_drink
 	HAVING beg_date >= F AND end_date <= T AND id_drink==input_drink AND 
 		LENGTH 
@@ -52,44 +65,39 @@
 		) > 0
 
 
-• знайти сумарну кiлькiсть втеч з витверезника по мiсяцях;
+-- • знайти сумарну кiлькiсть втеч з витверезника по мiсяцях;
 
-	SELECT COUNT(id_alc), EXTRACT(MONTH FROM when)
+	SELECT COUNT(id_alc), EXTRACT(MONTH FROM escape_date)
 	FROM escape
-	GROUP BY EXTRACT(MONTH FROM when)
+	GROUP BY EXTRACT(MONTH FROM escape_date)
 
 
-• вивести лiжка витверезника у порядку спадання середньої кiлькостi втрат свiдомостi для усiх алкоголiкiв, що були приведенi на лiжко iнспектором I за вказаний перiод (з дати F по дату T);
+-- • вивести лiжка витверезника у порядку спадання середньої кiлькостi втрат свiдомостi для усiх 
+--   алкоголiкiв, що були приведенi на лiжко iнспектором I за вказаний перiод (з дати F по дату T);
 
 
-	SELECT id_bed, AVG(id_unc)
-	FROM log
+	SELECT id_bed, ROUND(COUNT(id_unc)/COUNT(id_bed), 2)
+	FROM (log
 	JOIN unconsciousness
-	ON log.id_alc = unconsciousness.id_alc
-	WHERE id_insp_in == I AND beg_date >= F AND end_date <= T
+		ON log.id_alc = unconsciousness.id_alc) as log_unc
+	JOIN group_check_in
+		ON group_check_in.id_check_in = log_unc.id_group_check_in
+	WHERE inspector_in = 8 AND time >= '2020-04-29 12:00:00' AND end_date <= '2021-05-10 22:30:00' 
 	GROUP BY id_bed
-	ORDER BY AVG(id_unc) DESC
+	ORDER BY ROUND(COUNT(id_unc)/COUNT(id_bed), 2) DESC
 
 
-• вивести алкогольнi напої у порядку спадання сумарної кiлькостi алкоголiкiв, що його розпивала разом з алкоголiком A за вказаний перiод (з дати F по дату T);
+-- • вивести алкогольнi напої у порядку спадання сумарної кiлькостi алкоголiкiв, що його розпивала 
+--   разом з алкоголiком A за вказаний перiод (з дати F по дату T);
 
 	
 	SELECT id_drink
-	FROM group
+	FROM group_check_in
 	JOIN log
-	ON group.id_group = log.id_group
-	WHERE id_alc == A
-	ORDER BY AVG(LENGTH(
-			 (
-				SELECT COUNT(id_alc) FROM log
-				GROUP BY id_group
-				HAVING id_alc == A
-			 ))) DESC
-
-
-
-
-
-
-
-
+	ON group_check_in.id_check_in = log.id_group_check_in
+	
+	WHERE id_alc = 4
+	ORDER BY (
+				SELECT COUNT(*) FROM log
+				GROUP BY id_group_check_in
+			 ) DESC
